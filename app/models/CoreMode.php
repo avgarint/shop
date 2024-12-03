3d5d7a337557e51a5b35775994a91dc65a6506a8
avgarint <66020831+avgarint@users.noreply.github.com>
2024-12-03 18:37:23 +0100

    Update CoreMode.php

--- app/models/CoreMode.php
+++ app/models/CoreMode.php
@@ -4,6 +4,22 @@ class CoreModel {
     private $id;
     private $created_at;
     private $updated_at;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getCreatedAt() {
+        return $this->created_at;
+    }
+
+    public function getUpdatedAt() {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt($updated_at) {
+        $this->updated_at = $updated_at;
+    }
 }
 
 ?>
